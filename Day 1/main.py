77553f7fc74eb1675e536262b8a38fbbf70eb17d
Gustav Pettersson <GustavPettersson@live.se>
2022-12-01 21:53:47 +0100

    Day 1 done

---
+++ Day 1/main.py
@@ -0,0 +1,20 @@
+file = open("./input.txt")
+
+lines = file.readlines()
+elfCalories = [0]
+index = 0;
+
+for line in lines:
+    if not line.strip():
+        index += 1
+        elfCalories.append(0)
+        continue
+    
+    elfCalories[index] += int(line.strip())
+
+# Answer for first challenge
+print(max(elfCalories))
+
+elfCalories.sort(reverse=True)
+
+print(elfCalories[0] + elfCalories[1] + elfCalories[2])
